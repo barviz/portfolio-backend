74a300b6a425f3d14df56bd7eac20c811d990c26
barviz <barbaravizgarra@gmail.com>
2022-07-27 22:30:36 -0300

    seguridad: jwt agregados y funcionales

---
+++ src/main/java/com/proyecto/PortfolioArgProg/security/jwt/JwtProvider.java
@@ -0,0 +1,62 @@
+package com.proyecto.PortfolioArgProg.security.jwt;
+
+import com.proyecto.PortfolioArgProg.security.entity.MainUser;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Barbara
+ */
+@Component
+public class JwtProvider {
+    
+     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private int expiration;
+    
+    public String generateToken(Authentication authentication){
+        MainUser mainUser = (MainUser) authentication.getPrincipal();
+        return Jwts.builder().setSubject(mainUser.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+expiration*1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+    
+    public String getUserNameFromToken(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+    
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        }catch (MalformedJwtException e){
+            logger.error("Token mal formado");
+        }catch (UnsupportedJwtException e){
+            logger.error("Token no soportado");
+        }catch (ExpiredJwtException e){
+            logger.error("Token expirado");
+        }catch (IllegalArgumentException e){
+            logger.error("Token vacio");
+        }catch (SignatureException e){
+            logger.error("Firma no válida");
+        }
+        return false;
+    }
+    
+}
