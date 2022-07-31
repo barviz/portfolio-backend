8b500a26b8c528c0883ba9266b9e83a0583a3cc7
barviz <barbaravizgarra@gmail.com>
2022-07-30 21:55:39 -0300

    m

--- src/main/java/com/proyecto/PortfolioArgProg/security/controller/AuthController.java
+++ src/main/java/com/proyecto/PortfolioArgProg/security/controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://portfolio-blv.web.app")
 public class AuthController {
 
     @Autowired PasswordEncoder passwordEncoder;
